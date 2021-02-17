31b582620702a1f9596525e79c0e7f9d169238d4
StarSpace37 <70822218+StarSpace37@users.noreply.github.com>
2021-02-17 23:07:08 +0200

    Revert "Initial commit"
    This reverts commit 58f058b63b7dcb823db701b5d4d1484132978ed0.

--- nova_poshta.php
+++ nova_poshta.php
@@ -3,8 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
-    content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!--<link rel="stylesheet" href="css/select.css">
+	<link rel="stylesheet" href="css/style.css">-->
     <title>@yield('title')</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
@@ -21,10 +23,24 @@
 <div class="container mt-5">
     <main role="main">
 
+        <!-- Main jumbotron for a primary marketing message or call to action -->
         <div class="jumbotron bg-warning">
             <div class="container">
                 <form action="#" id="form" class="form__body">
                     <h1 class="form__title">Нова Пошта</h1>
+
+                    <!--<form method="post" action="/welcome/check">
+                    @csrf
+                    @if($errors->any())
+                    <div class="alert alert-damger">
+                    <ul>
+                    @foreach($error->all() as $error)
+                    <li>{{$error}}</li>
+                    @endforeach
+                    </ul>
+                    </div>
+                    @endif
+                    -->
                     <div class="form__item">
                         <input type="text" name="firstname" id="firstname" placeholder="Имя (получателя)" class="form-control"><br>
                         <input type="text" name="lastname" id="lastname" placeholder="Фамилия (получателя)" class="form-control"><br>
@@ -35,6 +51,33 @@
                         <input type="text" list="warehouses" name="office" id="office"  placeholder="Отделение «Нова Пошта»" class="form-control"><br>
                         <button type="submit" class="btn btn-success">Отправить</button>
                 </form>
+                <!--<div class="ms-box">
+                    <div class="ms-box-t">Адрес доставки «Нова Пошта»</div>
+                        <div class="field-for-group">
+                            <div class="input-label-group">
+                                <label class="label-common required">Область</label>
+                                <input class="form-control" data-autocomplete="np_region" type="text" name="region" id="customer_drop_lastname" value="" placeholder="" data-reload-payment-form="true">
+                                                        <div class="autocomplete active">
+                                        <div class="autocomplete-list"></div>
+                                    </div>
+                            </div>
+                            <div class="input-label-group">
+                                <label class="label-common required">Город</label>
+                                <input class="form-control" data-autocomplete="np_city" data-min="3" type="text" name="city" id="shipping_address_city" value="" placeholder="" data-onchange="reloadAll" autocomplete="off">
+                                                    <div class="autocomplete active">
+                                    <div class="autocomplete-list"></div>
+                                </div>
+                            </div>
+                            <div class="input-label-group">
+                                <label class="label-common required">Отделение «Нова Пошта»</label>
+                                <input class="form-control" type="text" name="office" id="shipping_address_address_1" value="" placeholder="" data-onchange="reloadAll" data-autocomplete="np_office">
+                                                <div class="autocomplete active">
+                                <div class="autocomplete-list"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>-->
                 </form>
             </div>
         </div>
